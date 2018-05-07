e3d23c8b5e5e62225fdeb902e9b23f0474c5b923
Alexandre Alapetite <alexandre@alapetite.fr>
2018-05-07 14:22:54 +0200

    Exclude raspicam from AMD64 builds

--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+arch=`dpkg --print-architecture`
+if [[ $arch =~ ^arm ]]; then arch="arm";
+elif [[ $arch =~ ^amd64 ]]; then arch="amd64";
+else echo "Unsupported architecture" >&2; exit 1; fi
+echo "Architecture: $arch"
+
 docker pull ros:kinetic-ros-base-xenial
 docker build --no-cache --tag dtur3/r3-tutorials ./r3-tutorials/
 docker build --tag dtur3/r3-arlobot ./r3-arlobot/
 docker build --tag dtur3/r3-teleop ./r3-teleop/
 docker build --tag dtur3/r3-mqtt-bridge ./r3-mqtt-bridge/
 docker build --tag dtur3/r3-navigation ./r3-navigation/
-docker build --tag dtur3/r3-raspicam ./r3-raspicam/
 docker build --tag dtur3/r3-fiducials ./r3-fiducials/
 docker build --tag dtur3/r3-ws-bridge ./r3-ws-bridge/
 docker build --tag dtur3/r3-node-red ./r3-node-red/
 docker build --tag dtur3/r3-navigation ./r3-navigation/
 docker build --tag dtur3/r3-odometry-control ./r3-odometry-control/
+
+if [ "$arch" == 'arm' ]; then
+	docker build --tag dtur3/r3-raspicam ./r3-raspicam/
+fi
--- docker-push.sh
+++ docker-push.sh
@@ -6,35 +6,37 @@ elif [[ $arch =~ ^amd64 ]]; then arch="amd64";
 else echo "Unsupported architecture" >&2; exit 1; fi
 echo "Architecture: $arch"
 
-sudo docker tag dtur3/r3-tutorials dtur3/r3-tutorials:$arch
-sudo docker push dtur3/r3-tutorials:$arch
+docker tag dtur3/r3-tutorials dtur3/r3-tutorials:$arch
+docker push dtur3/r3-tutorials:$arch
 
-sudo docker tag dtur3/r3-arlobot dtur3/r3-arlobot:$arch
-sudo docker push dtur3/r3-arlobot:$arch
+docker tag dtur3/r3-arlobot dtur3/r3-arlobot:$arch
+docker push dtur3/r3-arlobot:$arch
 
-sudo docker tag dtur3/r3-teleop dtur3/r3-teleop:$arch
-sudo docker push dtur3/r3-teleop:$arch
+docker tag dtur3/r3-teleop dtur3/r3-teleop:$arch
+docker push dtur3/r3-teleop:$arch
 
-sudo docker tag dtur3/r3-mqtt-bridge dtur3/r3-mqtt-bridge:$arch
-sudo docker push dtur3/r3-mqtt-bridge:$arch
+docker tag dtur3/r3-mqtt-bridge dtur3/r3-mqtt-bridge:$arch
+docker push dtur3/r3-mqtt-bridge:$arch
 
-sudo docker tag dtur3/r3-navigation dtur3/r3-navigation:$arch
-sudo docker push dtur3/r3-navigation:$arch
+docker tag dtur3/r3-navigation dtur3/r3-navigation:$arch
+docker push dtur3/r3-navigation:$arch
 
-sudo docker tag dtur3/r3-raspicam dtur3/r3-raspicam:$arch
-sudo docker push dtur3/r3-raspicam:$arch
+docker tag dtur3/r3-fiducials dtur3/r3-fiducials:$arch
+docker push dtur3/r3-fiducials:$arch
 
-sudo docker tag dtur3/r3-fiducials dtur3/r3-fiducials:$arch
-sudo docker push dtur3/r3-fiducials:$arch
+docker tag dtur3/r3-ws-bridge dtur3/r3-ws-bridge:$arch
+docker push dtur3/r3-ws-bridge:$arch
 
-sudo docker tag dtur3/r3-ws-bridge dtur3/r3-ws-bridge:$arch
-sudo docker push dtur3/r3-ws-bridge:$arch
+docker tag dtur3/r3-node-red dtur3/r3-node-red:$arch
+docker push dtur3/r3-node-red:$arch
 
-sudo docker tag dtur3/r3-node-red dtur3/r3-node-red:$arch
-sudo docker push dtur3/r3-node-red:$arch
+docker tag dtur3/r3-navigation dtur3/r3-navigation:$arch
+docker push dtur3/r3-navigation:$arch
 
-sudo docker tag dtur3/r3-navigation dtur3/r3-navigation:$arch
-sudo docker push dtur3/r3-navigation:$arch
+docker tag dtur3/r3-odometry-control dtur3/r3-odometry-control:$arch
+docker push dtur3/r3-odometry-control:$arch
 
-sudo docker tag dtur3/r3-odometry-control dtur3/r3-odometry-control:$arch
-sudo docker push dtur3/r3-odometry-control:$arch
+if [ "$arch" == 'arm' ]; then
+	docker tag dtur3/r3-raspicam dtur3/r3-raspicam:$arch
+	docker push dtur3/r3-raspicam:$arch
+fi
