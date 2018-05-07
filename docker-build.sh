2c2f0d74e878c54a0fcee4c06d11fa8f6ce2bb11
Alexandre Alapetite <alexandre@alapetite.fr>
2018-05-07 14:28:04 +0200

    Minor whitespace

--- docker-build.sh
+++ docker-build.sh
@@ -7,7 +7,9 @@ else echo "Unsupported architecture" >&2; exit 1; fi
 echo "Architecture: $arch"
 
 docker pull ros:kinetic-ros-base-xenial
+
 docker build --no-cache --tag dtur3/r3-tutorials ./r3-tutorials/
+
 docker build --tag dtur3/r3-arlobot ./r3-arlobot/
 docker build --tag dtur3/r3-teleop ./r3-teleop/
 docker build --tag dtur3/r3-mqtt-bridge ./r3-mqtt-bridge/
