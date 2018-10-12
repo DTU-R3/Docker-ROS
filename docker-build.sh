3fed7d2abb0cac1a5b5f7350bbe63782e5366a09
Alexandre Alapetite <alexandre@alapetite.fr>
2018-10-12 11:45:29 +0200

    Add manifest fiducials

--- docker-build.sh
+++ docker-build.sh
@@ -13,8 +13,8 @@ docker build --no-cache --tag dtur3/r3-tutorials ./r3-tutorials/
 docker build --tag dtur3/r3-arlobot ./r3-arlobot/
 docker build --tag dtur3/r3-teleop ./r3-teleop/
 docker build --tag dtur3/r3-mqtt-bridge ./r3-mqtt-bridge/
-docker build --tag dtur3/r3-navigation ./r3-navigation/
 docker build --tag dtur3/r3-fiducials ./r3-fiducials/
+docker build --tag dtur3/r3-navigation ./r3-navigation/
 docker build --tag dtur3/r3-ws-bridge ./r3-ws-bridge/
 docker build --tag dtur3/r3-node-red ./r3-node-red/
 
